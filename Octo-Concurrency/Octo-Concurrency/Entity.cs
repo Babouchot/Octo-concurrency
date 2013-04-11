3a8f92ba45ec0c12c902d097b18c9922db0ae94a
Maurogik <gwenn.aubert@gmail.com>
2013-04-11 21:45:09 +0200

    ajout des méthodes d'accés au éléments d'Entity et ajout du calcul du prochain mouvement dans Entity

--- Octo-Concurrency/Octo-Concurrency/Entity.cs
+++ Octo-Concurrency/Octo-Concurrency/Entity.cs
@@ -7,20 +7,74 @@ namespace OctoConcurrency
 	{
 		private Vector2 position;
 		private Vector2 destination;
-		private int taille;
-		private World world;
+		private int radius;
+		private float maxSpeed;
+		//private World world; 
+		//WHY ?
 
 		public Entity ()
 		{
 			position = new Vector2();
 			destination = new Vector2();
-			taille = 0;
-			world = new World();
+			radius = 1;
+			//world = new World();
 		}
 
 		public Entity(Vector2 position, Vector2 destination) {
 			this.position = position;
 			this.destination = destination;
+		}
+
+		public float Radius {
+			get { return radius; }
+			set { radius = value; }
+		}
+
+		public Vector2 Position{
+			get { return position; }
+			set { position = value; }
+		}
+
+		public Vector2 Destination{
+			get { return destination; }
+			set { Console.Out.WriteLine("Deprecated fuction ! " +
+				"Bad boy ! (check with Gwen if you don't understad why");
+				destination = value; }
+		}
+
+		public Vector2 calculateNextMove(){
+			calculateNextMove(0);
+		}
+
+		/**
+		 * Calculate the next position
+		 * The calculated move will be redirected according to the rotationOffset ( 0 = none 1 = full turn)
+		 * Rotation Offset should be between -1 and 1
+		 */
+		public Vector2 calculateNextPos(float rotationOffset){
+			if(rotationOffset > 1 || rotationOffset < -1){
+				//throw ArgumentException;
+			}
+
+			//Calculate and move down to maxSpeed if needed
+			Vector2 tempMove = destination - position;
+			Console.Out.WriteLine("the line right after this might not work as intended");
+			tempMove = tempMove.Length() <= maxSpeed ? tempMove : tempMove.Normalize() * maxSpeed;
+
+			//Rotate the move vector by rotationOffset * Pi
+			tempMove = RotateVector2(tempMove, rotationOffset * Math.PI);
+
+			return tempMove;
+		}
+
+		private static Vector2 RotateVector2(Vector2 point, float radians)
+		{
+			float cosRadians = (float)Math.Cos(radians);
+			float sinRadians = (float)Math.Sin(radians);
+			
+			return new Vector2(
+				point.X * cosRadians - point.Y * sinRadians,
+				point.X * sinRadians + point.Y * cosRadians);
 		}
 	}
 }
